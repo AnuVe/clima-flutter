e0ba27f9fb5ea703c854f9aeb7726d795302787a
Anushka Verma <anushkavermatheprincess@gmail.com>
2020-09-29 21:53:58 +0530

    weather API integrated

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const appKey = 'f411919918d57d807fa6bd30d9294ddc';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,6 +11,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double lat;
+  double lon;
   @override
   void initState() {
     super.initState();
@@ -16,8 +22,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getLocation();
-    print(location.lat);
-    print(location.lon);
+    lat = location.lat;
+    lon = location.lon;
+    getData();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$appKey');
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var id = jsonDecode(data)['weather'][0]['id'];
+      var temp = jsonDecode(data)['main']['temp'];
+      var name = jsonDecode(data)['name'];
+      print(id);
+      print(temp);
+      print(name);
+    } else
+      print(response.statusCode);
   }
 
   @override
